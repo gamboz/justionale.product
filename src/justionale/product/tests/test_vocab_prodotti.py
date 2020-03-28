951043ace1c4e29d41a09ad2b6adcf8335b8bbef
Matteo Gamboz <gamboz@medialab.sissa.it>
2020-03-28 14:31:15 +0100

    "Add vocabulary: Prodotti"

---
+++ src/justionale/product/tests/test_vocab_prodotti.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from justionale.product import _
+from justionale.product.testing import JUSTIONALE_PRODUCT_INTEGRATION_TESTING  # noqa
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from zope.component import getUtility
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.interfaces import IVocabularyTokenized
+
+import unittest
+
+
+class ProdottiIntegrationTest(unittest.TestCase):
+
+    layer = JUSTIONALE_PRODUCT_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_vocab_prodotti(self):
+        vocab_name = 'justionale.product.Prodotti'
+        factory = getUtility(IVocabularyFactory, vocab_name)
+        self.assertTrue(IVocabularyFactory.providedBy(factory))
+
+        vocabulary = factory(self.portal)
+        self.assertTrue(IVocabularyTokenized.providedBy(vocabulary))
+        self.assertEqual(
+            vocabulary.getTerm('sony-a7r-iii').title,
+            _(u'Sony Aplha 7R III'),
+        )
